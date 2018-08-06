c92e5f35fba9c570b5b41a377e6d269de1db1c02
luqimin <luqimin@sogou-inc.com>
2018-08-06 23:29:01 +0800

    feat: 添加client服务接口数据mock功能

--- src/lib/client/devServer.ts
+++ src/lib/client/devServer.ts
@@ -3,13 +3,17 @@
  */
 import * as webpack from 'webpack';
 import * as path from 'path';
+import * as fs from 'fs';
 import * as WebpackDevServer from 'webpack-dev-server';
+import * as express from 'express';
 
 import Base from './Base';
 import log from '../../util/log';
+import { readFile } from '../../util/readFile';
 
 class DevServer extends Base {
     protected taskRunning() {
+        const that = this;
         // webpack compiler
         const entry: webpack.Entry = this.webpackConfig.entry as webpack.Entry;
 
@@ -21,6 +25,50 @@ class DevServer extends Base {
                 },
                 contentBase: this.runtime.clientPath,
                 publicPath: '/dist/',
+
+                /**
+                 * 添加mock数据接口功能
+                 */
+                before(app: express.Application): void {
+                    app.get(
+                        '/:path*',
+                        (
+                            req: express.Request,
+                            res: express.Response,
+                            next: express.NextFunction
+                        ) => {
+                            const _path: string = req.params['path'];
+                            const _tail: string = req.params['0'];
+                            // 不mock静态文件
+                            if (_path === 'dist') {
+                                next();
+                                return;
+                            }
+                            // mock目录client/mock里面所有json文件，支持多级目录
+                            const mockFilePath: string = _tail
+                                ? path.join(
+                                      that.runtime.clientPath || '',
+                                      'mock',
+                                      _path,
+                                      _tail.endsWith('.json')
+                                          ? _tail
+                                          : `${_tail}.json`
+                                  )
+                                : path.join(
+                                      that.runtime.clientPath || '',
+                                      'mock',
+                                      _path.endsWith('.json')
+                                          ? _path
+                                          : `${_path}.json`
+                                  );
+                            if (fs.existsSync(mockFilePath)) {
+                                res.json(readFile(mockFilePath));
+                            } else {
+                                next();
+                            }
+                        }
+                    );
+                },
             },
             this.webpackConfig.devServer
         );
